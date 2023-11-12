6bb871617bf1d222b6124bb0f07d7ec96d87e0b6
Lunguleasa Eugen Alexandru <93145279+Trefla25@users.noreply.github.com>
2023-11-12 16:42:29 +0200

    Added getData function

--- main.c
+++ main.c
@@ -16,9 +16,41 @@ typedef struct {
     time_t lastChange;
 }myStats; 
 
+void getData(int in, myStats *stat, char *numeFisier) {
+    struct stat fileStats;
+    int buffer;
 
+    strcpy(stat->numeFisier, numeFisier);
 
+    lseek(in, 18, SEEK_SET);
+    read(in, &buffer, 4);
+    stat->dimensiune = buffer;
 
+    read(in, &buffer, 4);
+    stat->L = buffer;
+
+    read(in, &buffer, 4);
+    stat->h = buffer;
+
+    fstat(in, &fileStats);
+    stat->countLegaturi = fileStats.st_nlink;
+    stat->userId = fileStats.st_uid;
+    stat->lastChange = fileStats.st_mtime;
+
+    (S_ISDIR(fileStats.st_mode)) ? strcpy(stat->drUser, "d") : strcpy(stat->drUser, "-");
+    (fileStats.st_mode & S_IRUSR) ? strcat(stat->drUser, "r") : strcat(stat->drUser, "-");
+    (fileStats.st_mode & S_IWUSR) ? strcat(stat->drUser, "w") : strcat(stat->drUser, "-");
+    (fileStats.st_mode & S_IXUSR) ? strcat(stat->drUser, "x") : strcat(stat->drUser, "-");
+
+    (fileStats.st_mode & S_IRGRP) ? strcpy(stat->drGrup, "r") : strcpy(stat->drGrup, "-");
+    (fileStats.st_mode & S_IRGRP) ? strcat(stat->drGrup, "w") : strcat(stat->drGrup, "-");
+    (fileStats.st_mode & S_IXGRP) ? strcat(stat->drGrup, "x") : strcat(stat->drGrup, "-");
+
+    (fileStats.st_mode & S_IROTH) ? strcpy(stat->drOthers, "r") : strcpy(stat->drOthers, "-");
+    (fileStats.st_mode & S_IWOTH) ? strcat(stat->drOthers, "w") : strcat(stat->drOthers, "-");
+    (fileStats.st_mode & S_IXOTH) ? strcat(stat->drOthers, "x") : strcat(stat->drOthers, "-");
+
+}
 
 
 
@@ -43,6 +75,9 @@ int main(int argc, char **argv){
         printf("Couldn t open output file!\n");
         exit(1);
     }
+
+    close(in_file);
+    close(out_file);
 
 
     return 0;
