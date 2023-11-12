dc0341214a6fd586772619d596c344882f978f3d
Lunguleasa Eugen Alexandru <93145279+Trefla25@users.noreply.github.com>
2023-11-12 20:18:10 +0200

    Added getData
    To be finished Monday

--- S7/main.c
+++ S7/main.c
@@ -18,36 +18,91 @@ typedef struct {
     time_t lastChange;
 }myStats; 
 
+void getData(int in, myStats *stat, char *numeFisier) {
+    struct stat fileStats;
+    int buffer;
+
+    strcpy(stat->numeFisier, numeFisier);
+
+    lseek(in, 18, SEEK_SET);
+    read(in, &buffer, 4);
+    stat->dimensiune = buffer;
+
+    read(in, &buffer, 4);
+    stat->L = buffer;
+
+    read(in, &buffer, 4);
+    stat->h = buffer;
+
+     if (strstr(numeFisier, ".bmp")) {
+        read(in, &buffer, 4);
+        stat->L = buffer;
+
+        read(in, &buffer, 4);
+        stat->h = buffer;
+    }
+    else {
+        stat->L = -1;
+        stat->h = -1;
+    }
+
+    fstat(in, &fileStats);
+    stat->countLegaturi = fileStats.st_nlink;
+    stat->userId = fileStats.st_uid;
+    stat->lastChange = fileStats.st_mtime;
+
+    (S_ISDIR(fileStats.st_mode)) ? strcpy(stat->drUser, "d") : strcpy(stat->drUser, "-");
+
+    (fileStats.st_mode & S_IRUSR) ? strcat(stat->drUser, "r") : strcat(stat->drUser, "-");
+    (fileStats.st_mode & S_IWUSR) ? strcat(stat->drUser, "w") : strcat(stat->drUser, "-");
+    (fileStats.st_mode & S_IXUSR) ? strcat(stat->drUser, "x") : strcat(stat->drUser, "-");
+
+    (fileStats.st_mode & S_IRGRP) ? strcpy(stat->drGrup, "r") : strcpy(stat->drGrup, "-");
+    (fileStats.st_mode & S_IRGRP) ? strcat(stat->drGrup, "w") : strcat(stat->drGrup, "-");
+    (fileStats.st_mode & S_IXGRP) ? strcat(stat->drGrup, "x") : strcat(stat->drGrup, "-");
+
+    (fileStats.st_mode & S_IROTH) ? strcpy(stat->drOthers, "r") : strcpy(stat->drOthers, "-");
+    (fileStats.st_mode & S_IWOTH) ? strcat(stat->drOthers, "w") : strcat(stat->drOthers, "-");
+    (fileStats.st_mode & S_IXOTH) ? strcat(stat->drOthers, "x") : strcat(stat->drOthers, "-");
+
+}
+
 
 int main(int argc, char **argv){
     myStats stats; 
     struct stat fStat;
     struct dirent *dirent;
-    // DIR *dir
+    DIR *dir
 
-    if (argc != 2 || strstr(argv[1], ".bmp") == NULL) {
-        printf("Usage ./program <fisier_intrare>\n");
+    if (argc != 2) {
+        printf("Usage ./program <director intrare>\n");
         exit(1);
     }
 
-    int in_file = open("image.bmp", O_RDONLY);
-
-    if (in_file == -1) {
-        printf("Couldn t open input file!\n");
+    dir = opendir(argv[1]);
+    if (dir == NULL) {
+        printf("Error\n");
         exit(1);
     }
 
-    int out_file = open("statistica.txt", O_WRONLY);
+    while ((dirent = readdir(dir)) != NULL) {
+        char path[50];
+        strcpy(path, argv[1]);
+        strcat(path, "/");
+        strcat(path, dirent->d_name);
+        
+        if ((stat(path, &fStat) == -1)) {
+            printf("Error\n");
+            exit(1);
+        }
 
-    if (out_file == -1) {
-        printf("Couldn t open output file!\n");
-        exit(1);
+        // getData();
+        // close(in_file);
+        // close(out_file);
+
     }
 
-  //  getData(in_file, &stats, argv[1]);
-  //  fPrint(stats, out_file);
+    closedir(dir);
 
-    close(in_file);
-    close(out_file);
     return 0;
 }
