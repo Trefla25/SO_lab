0a0c61a49b728cda25bc0825c065c7dd0364eb04
Lunguleasa Eugen Alexandru <93145279+Trefla25@users.noreply.github.com>
2023-11-12 14:47:06 +0200

    Add files via upload
    main.c was created and added

---
+++ main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<time.h>
+
+
+int main(int argc, char **argv){
+
+    return 0;
+}
